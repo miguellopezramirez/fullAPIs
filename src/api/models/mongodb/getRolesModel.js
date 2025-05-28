adfec28fa38c3d0af17c50d5557e4634eae70dd6
DulceArvizu <163905040+DulceArvizu@users.noreply.github.com>
2025-05-28 07:39:39 -0700

    Back de roles
    Cambio de todo el back de roles, sin vistas enriquecidas ya

--- src/api/models/mongodb/getRolesModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const RolesInfoSchema = new mongoose.Schema({}, { strict: false }); 
-
-const RolesInfo = mongoose.model('roles_info', RolesInfoSchema, 'roles_info'); 
-
-module.exports = RolesInfo;
--- src/api/models/mongodb/getRolesUsersModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const RolesInfoUsersSchema = new mongoose.Schema({}, { strict: false }); 
-
-const RolesInfoUsers = mongoose.model('roles_users', RolesInfoUsersSchema, 'roles_users'); 
-
-
-module.exports = RolesInfoUsers;
